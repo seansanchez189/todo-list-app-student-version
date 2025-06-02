7efeb4e963bba70227719f39001dba6969a2ed7d
Jordans249 <jordan.scott@bronxsoftware.org>
2025-06-02 14:11:49 -0400

    Update tasks.js

--- backend/routes/tasks.js
+++ backend/routes/tasks.js
@@ -3,15 +3,14 @@ const router = express.Router();
 // There is a bug in line 4 you need to fix it
 const taskModel = require("../models/taskModel");
 
-//Write a comment describing the purpose of this route ~ the purpose of this route is to get request through the root path, fetch a lists through taskModel, then return the list back to JSON Object
+//the purpose of this route is to get request through the root path, fetch a lists through taskModel, then return the list back to JSON Object
 router.get("/", async (req, res) => {
   const tasks = await taskModel.getTasks();
   res.json(tasks);
 });
 
-// Write a comment describing the purpose of this route
+// The purpose of this route is to accept incoming request of name and description, then add a task to taskmodel, then respond through the status code
 router.post("/", async (req, res) => {
-  //there is a bug in line 15 you need to fix
   const { name, description } = req.body;
   const task = await taskModel.addTask(name, description);
   res.status(201).json(task);
